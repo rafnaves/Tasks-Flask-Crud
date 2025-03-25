37e31ef257f91990af5daf1f786f9a745c12855a
rafnaves <rafaelnavesdev@gmail.com>
2025-03-24 21:26:21 -0300

    Novas rotas

--- app.py
+++ app.py
@@ -38,5 +38,37 @@ def get_task(id):
             return jsonify(t.to_dict())
     return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+        
+    if task == None:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data["description"]
+    task.completed = data['completed']
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+    if task == None:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
